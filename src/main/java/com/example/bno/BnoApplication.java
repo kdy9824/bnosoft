1ed89a4ea2be5f5f9ab0be0f1c98cf4bb5136c6f
kdy9824 <dyk2098@naver.com>
2024-04-15 17:29:42 +0900

    Initial commit

---
+++ src/main/java/com/example/bno/BnoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BnoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bno/BnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
