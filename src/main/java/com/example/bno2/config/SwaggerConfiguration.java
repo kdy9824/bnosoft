ded2ada9482068ac4d5af16d7a089539f86f5e96
kdy9824 <dyk2098@naver.com>
2024-04-22 10:28:49 +0900

    2024-04-22
    SpringBoot swagger 추가

---
+++ src/main/java/com/example/bno2/config/SwaggerConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.bno2.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+    public static final Info BNO_BACKEND_API_INFO = new Info()
+            .title("타이틀")
+            .description("설명")
+            .version("v0.0.1");
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI().info(BNO_BACKEND_API_INFO);
+    }
+
+
+}
--- src/main/java/com/example/bno2/controller/EquipmentController.java
+++ src/main/java/com/example/bno2/controller/EquipmentController.java
@@ -2,6 +2,8 @@ package com.example.bno2.controller;
 
 import com.example.bno2.model.Equipment;
 import com.example.bno2.service.EquipmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,12 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+@Tag(name="장비")
 @Controller
 public class EquipmentController {
+
 
     @Autowired
     private EquipmentService equipmentService;
 
+    @Operation(summary = "dddd")
     @GetMapping("/equipment")
     public String equipment(Model model) {
 
