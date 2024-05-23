e703c5b9d83548dcf36d5e85b7a519762f629ce9
kdy9824 <dyk2098@naver.com>
2024-05-23 13:07:43 +0900

    2024-05-23 13:05
    로그인 -> OTP 등록 시 QR코드 이미지 출력 성공

--- src/main/java/com/example/bno2/service/impl/OtpServiceImpl.java
+++ src/main/java/com/example/bno2/service/impl/OtpServiceImpl.java
@@ -4,23 +4,19 @@ import com.example.bno2.dto.User;
 import com.example.bno2.mapper.LoginMapper;
 import com.example.bno2.mapper.OtpMapper;
 import com.example.bno2.service.OtpService;
-import com.example.bno2.utils.ImageToBlob;
 import com.example.bno2.utils.OTPUtil;
 import com.google.zxing.WriterException;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.*;
 import java.util.Base64;
 
 @Service
@@ -32,60 +28,7 @@ public class OtpServiceImpl implements OtpService {
     @Autowired
     LoginMapper loginMapper;
 
-    public ImageToBlob imageToBlob = new ImageToBlob();
     public OTPUtil otpUtil = new OTPUtil();
-
-//    @Override
-//    public ResponseEntity<byte[]> otpRegister(HttpSession session) throws IOException, WriterException {
-//
-//        User loginUser = (User)session.getAttribute("loginUser");
-//        int userPn = loginUser.getPn();
-//
-//        if(otpMapper.checkQrBlob(userPn) == 0){
-//
-//            if(otpMapper.checkSecretKey(userPn) == 0)
-//                otpMapper.addSecretKey(userPn, otpUtil.getSecretKey());
-//
-//            String filePath = System.getProperty("user.home") + "/Desktop/image";
-//            File folder = new File(filePath);
-//
-//            if(!folder.exists())
-//                folder.mkdirs();
-//
-//            if(!folder.canWrite())
-//                folder.setWritable(true);
-//
-//            otpUtil.getQRImage(otpUtil.getGoogleOTPAuthURL(otpMapper.getSecretKey(userPn), loginUser.getEmail(), "bnosoft"),filePath+"/" + userPn +"_QR.png", 900, 900);
-//            byte[] blob = imageToBlob.convertImageToBlob(filePath+"/" + userPn +"_QR.png");
-//            otpMapper.addQrBlob(userPn,blob);
-//
-//        }
-//
-//        String url = "jdbc:mysql://124.63.21.91:5555/bnodb?useSSL=false&characterEncoding=UTF-8&serverTimezone=UTC&allowPublicKeyRetrieval=true";
-//        String username = "admin";
-//        String dbPassword = "admin";
-//        String sql = "SELECT qr_blob FROM otp_regist WHERE user_pn = ?";
-//
-//        try (Connection conn = DriverManager.getConnection(url, username, dbPassword);
-//            PreparedStatement pstmt = conn.prepareStatement(sql)) {
-//
-//            pstmt.setInt(1, userPn);
-//
-//            try (ResultSet rs = pstmt.executeQuery()) {
-//                if (rs.next()) {
-//                    Blob blob = rs.getBlob("qr_blob");
-//
-//                    InputStream inputStream = blob.getBinaryStream();
-//
-//                    return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(inputStream.readAllBytes());
-//                } else
-//                    return null;
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            return null;
-//        }
-//    }
 
     @Override
     public ResponseEntity<String> otpRegister(HttpSession session) throws IOException, WriterException {
